3e9c8c603b75f3021d44b8d49fff057943f3819e
Sadeepa-dilshan <mgsdilshan5@gmail.com>
2023-06-30 10:02:17 +0530

    Initial commit

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -8,6 +8,7 @@ function About() {
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                  Cumque sed est commodi doloremque maxime optio explicabo,
                  blanditiis esse iste quis laboriosam.</p>
+                 <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt=""/>
         </div>
 
     </>
