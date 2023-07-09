3413f8755b9e78a4cbeed60265df3c1142b1fb7b
Sadeepa-dilshan <S19002535@ousl.lk>
2023-07-09 10:16:52 +0530

    Revert "add extra lorem word to p in about page"
    This reverts commit cb662388170a54ac328380260c4f4edaf1ad91b8.

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <>
         <div className="container mt-4">
             <h3>About us</h3>
-            <p>Lorem lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                  Cumque sed est commodi doloremque maxime optio explicabo,
                  blanditiis esse iste quis laboriosam.</p>
                  <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt=""/>
